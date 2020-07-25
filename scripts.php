e553474c28c392cc7ec9d32bdaffb95a4eadec4f
avelok <mraurelius@yandex.ru>
2020-07-25 21:37:46 +0300

    script

--- scripts.php
+++ scripts.php
@@ -1,1 +1,23 @@
 <?php
+
+
+function checkData()
+{
+    global $doc;
+    $y = $_POST['y'];
+    $r = $_POST['r'];
+    $x = $_POST['rangeSlider'];
+
+    if ($y <= -1 || $y >= 3) {
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        echo '<style type="text/css">
+        #y {
+            display: none;
+        }
+        </style>';
+    }
+
+}
+
+checkData();
+
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,12 @@
 function checkYData() {
     let y = document.getElementById("y").value.replace(",", ".")
 
+    if (y === "-") {
+        return;
+    }
     if (parseInt(y)) {
-        if (y > -3 && y < 3) {
+        if (y >= -3 && y <= 3) {
             document.getElementById("y").setAttribute("style", "border: 2px solid green;")
-            console.log("q")
         } else {
             document.getElementById("y").setAttribute("style", "border: 2px solid red;")
             document.getElementById("y").value = "";
@@ -19,7 +21,7 @@ function checkRData() {
     let r = document.getElementById("r").value.replace(",", ".")
 
     if (parseInt(r)) {
-        if (r > -1 && r < 4) {
+        if (r >= 1 && r <= 4) {
             document.getElementById("r").setAttribute("style", "border: 2px solid green;")
             console.log("q")
         } else {
